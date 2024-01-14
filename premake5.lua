ace623d1c77d1bb8ac8c9612525f2e67a766415f
nofear <nofearsheenhere@gmail.com>
2024-01-14 09:35:42 +0530

    Added linux binary for premake5

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,8 @@ workspace "WalnutApp"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "WalnutApp"
+   runpathdirs { "cmake" }
+   require "cmake"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
